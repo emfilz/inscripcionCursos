6951937fb341d59610d71556f34ba6df958b13a0
Marcia Filz <marciafilz@gmail.com>
2022-07-16 15:14:30 -0300

    subida de la primera instancia del trabajo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-let botonInscripcion = document.getElementById("botonInscripcion");
-botonInscripcion.addEventListener("click", inscribir);
+function botonFormulario(){
+    let botonInscripcion = document.getElementById("botonInscripcion");
+    botonInscripcion.addEventListener("click", inscribir);
+}
 
 function inscribir(){
     let formulario = [];
@@ -22,31 +24,75 @@ function inscribir(){
     let datosCorrectos = true;
     for (const campo of formulario){    
         if(campo.valor == ""){
-            alert(`Es necesario completar el campo ${campo.campo}`);
+            Swal.fire({
+                icon: 'error',
+                title: 'Por favor completa todos los datos',
+                text: `El dato ${campo.campo} esta vacio`,
+                footer: 'Los datos los vamos a utilizar para armar tu ficha de estudiante'
+              })              
             datosCorrectos = false;
             break;
         }
     }
 
+    let planElegido = "";
     if(datosCorrectos){
         if(document.getElementById("plan1").checked){
-            formulario.push({campo: 'Plan de pago elegido', valor: `Plan regular mes a mes`});
+            planElegido = `Plan regular mes a mes`;
+            formulario.push({campo: 'Plan de pago elegido', valor: planElegido});
         }else if(document.getElementById("plan2").checked){
-                formulario.push({campo: 'Plan de pago elegido', valor: `Suscripción mensual`});
+                planElegido = `Suscripción mensual`;
+                formulario.push({campo: 'Plan de pago elegido', valor: planElegido});
         }else{
-            alert(`Es necesario elegir un plan de pago`);
+            Swal.fire({
+                icon: 'error',
+                title: 'Por favor completa todos los datos',
+                text: `No elegiste tu Plan de Pago`,
+                footer: 'Los datos los vamos a utilizar para armar tu ficha de estudiante'
+              })  
             datosCorrectos = false;
         }
     }
-  
-    if (datosCorrectos){
-        let ficha = ``;
-        for (campo of formulario){
-            ficha = ficha + `<p><span class="respuesta_campo">${campo.campo}: </span><span class="respuesta_valor">${campo.valor}</span></p>`;
-        }
-        document.getElementById(`respuesta_ficha`).innerHTML = ficha;
 
-        document.getElementById(`respuesta`).classList.remove(`invisble`);
-        document.getElementById(`respuesta`).classList.add(`visible`);
+    datosCorrectos ? confirmar(planElegido, formulario) : "";
+
+}
+
+function confirmar(planElegido, formulario){
+    const maxInscripciones = 1000;
+    Swal.fire({
+        title: 'Ya tenemos todos tus datos',
+        text: `Elegiste el Plan de Pago ${planElegido}`,
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Modificar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+                .then( (res) => res.json())
+                .then( (data) => {
+                    if (data.length < maxInscripciones){
+                        emailjs.sendForm("service_vxcc1fw", "template_vmtcb1r", document.getElementById("formularioInsc"), "QphrKaQYBCnDdJbkf")
+                            .then(function(response) {
+                                sessionStorage.setItem("FormularioInscripcion",JSON.stringify(formulario));
+                                window.location.href = "./fichadeestudiante.html";
+                            }, function(error) {
+                                console.log('FAILED...', error);
+                        });
+                    }else{
+                        alert(data.length)
+                    }
+            })
+        }
+    })
+}
+
+function fichaEstudiante(){
+    const formulario = JSON.parse(sessionStorage.getItem("FormularioInscripcion"));
+    let ficha = ``;
+    for (campo of formulario){
+        ficha = ficha + `<p><span class="respuesta_campo">${campo.campo}: </span><span class="respuesta_valor">${campo.valor}</span></p>`;
     }
+    document.getElementById(`respuesta_ficha`).innerHTML = ficha;
 }
